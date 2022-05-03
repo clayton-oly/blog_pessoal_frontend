ad8385a69d0f1852400585febca5ff25accaf507
Clayton <clayton.will08@gmail.com>
2022-05-03 19:32:49 -0300

    Temas e Postagens

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 
 function App() {
@@ -13,7 +15,7 @@ function App() {
 
     <Router>
       <Navbar />
-        <div >
+        <div style={{minHeight: '100vh'}}>
           <Routes>
             <Route path='/' element={<Home/>}/>
 
@@ -22,6 +24,10 @@ function App() {
             <Route path='/home' element={<Home/>}/>
 
             <Route path='/cadastrousuario' element={<CadastroUsuario/>}/>
+
+            <Route path='/temas' element={<ListaTema/>}/>
+
+            <Route path='/posts' element={<ListaPostagem/>}/>
           </Routes>
         </div>
       <Footer />
